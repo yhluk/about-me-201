5a4088dfed884700772efdff7a6956a28c4218cc
yurii hlukhyi <yhlukhyi@gmail.com>
2022-11-30 09:16:52 -0800

    added console lol

--- js/app.js
+++ js/app.js
@@ -6,42 +6,52 @@ console.log("user name=" + userName);
 let qOne = prompt('Was I born before 2000').toLowerCase();
 if (qOne === 'yes' || qOne === 'y') {
   alert('Correct');
+  // console.log('Correct');
 } else if (qOne === 'no' || qOne === 'n') {
   alert('Sorry, but No');
 }
+// console.log(Sorry, but No);
 console.log('question1 =' + qOne);
 
 
 let qTwo = prompt('Do I like wrestling?').toLowerCase();
 if (qTwo === 'yes' || qTwo === 'y') {
   alert('Correct');
+  // console.log('Correct');
 } else if (qTwo === 'no' || qTwo === 'n') {
   alert('Sorry, but No');
 }
+// console.log(Sorry, but No);
 console.log('question1 =' + qTwo);
 
 let qThree = prompt('Was I born in Peru').toLowerCase();
 if (qThree === 'yes' || qThree === 'y') {
   alert('Correct');
+  // console.log('Correct');
 } else if (qThree === 'no' || qThree === 'n') {
   alert('Sorry, but No');
 }
+// console.log(Sorry, but No);
 console.log('question1 =' + qThree);
 
 let qFour = prompt('Did I ever play soccer').toLowerCase();
 if (qFour === 'yes' || qFour === 'y') {
   alert('Correct');
+  // console.log('Correct');
 } else if (qFour === 'no' || qFour === 'n') {
   alert('Sorry, but No');
 }
+// console.log(Sorry, but No);
 console.log('question1 =' + qFour);
 
 let qFive = prompt('Am I trying to stay in shape?').toLowerCase();
 if (qFive === 'yes' || qFive === 'y') {
   alert('Correct');
+  // console.log('Correct');
 } else if (qFive === 'no' || qFive === 'n') {
   alert('Sorry, but No');
 }
+// console.log(Sorry, but No);
 console.log('question1 =' + qFive);
 
 alert(`Thanks ${userName} this survey was extremley fun`);
