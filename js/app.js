bcc55f56eda30c1e970ef9547571c592e1755303
yurii hlukhyi <yhlukhyi@gmail.com>
2022-11-30 00:23:58 -0800

    123

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-'use strict';
+// 'use strict';
 let userName = prompt('Who are you stranger?');
 alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
+console.log("user name=" ${userName});
 
 let qOne = prompt('Was I born before 2000').toLowerCase();
 if(qOne === 'yes' || qOne === 'y'){
   alert('Correct');
 } else if(qOne === 'no' || qOne === 'n'){
   alert('Sorry, but No');
-  else("Didn't answer it correctly")
+  else("Didn't answer it properly");
+  console.log()
 
