1969b003925d3b2accdcb24e10fea71b880bba01
yurii hlukhyi <yhlukhyi@gmail.com>
2022-11-30 19:41:38 -0800

    add 7th question

--- js/app.js
+++ js/app.js
@@ -1,61 +1,67 @@
 'use strict';
+let score = 0;
 
-// let userName = prompt('Who are you stranger?');
-// alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
-// console.log("user name=" + userName);
+let userName = prompt('Who are you stranger?');
+alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
+console.log("user name=" + userName);
 
-// // let score = 0;
+// let score = 0;
 
-// let qOne = prompt('Was I born before 2000').toLowerCase();
-// if (qOne === 'yes' || qOne === 'y') {
-//   alert('Correct');
-//   // console.log('Correct');
-// } else if (qOne === 'no' || qOne === 'n') {
-//   alert('Sorry, but No');
-// }
-// // console.log(Sorry, but No);
-// console.log('question1 =' + qOne);
+let qOne = prompt('Was I born before 2000').toLowerCase();
+if (qOne === 'yes' || qOne === 'y') {
+  alert('Correct');
+  score++;
+  // console.log('Correct');
+} else if (qOne === 'no' || qOne === 'n') {
+  alert('Sorry, but No');
+}
+// console.log(Sorry, but No);
+console.log('question1 =' + qOne);
 
 
-// let qTwo = prompt('Do I like wrestling?').toLowerCase();
-// if (qTwo === 'yes' || qTwo === 'y') {
-//   alert('Correct');
-//   // console.log('Correct');
-// } else if (qTwo === 'no' || qTwo === 'n') {
-//   alert('Sorry, but No');
-// }
-// // console.log(Sorry, but No);
-// console.log('question1 =' + qTwo);
+let qTwo = prompt('Do I like wrestling?').toLowerCase();
+if (qTwo === 'yes' || qTwo === 'y') {
+  alert('Correct');
+  score++;
+  // console.log('Correct');
+} else if (qTwo === 'no' || qTwo === 'n') {
+  alert('Sorry, but No');
+}
+// console.log(Sorry, but No);
+console.log('question1 =' + qTwo);
 
-// let qThree = prompt('Was I born in Peru').toLowerCase();
-// if (qThree === 'yes' || qThree === 'y') {
-//   alert('Correct');
-//   // console.log('Correct');
-// } else if (qThree === 'no' || qThree === 'n') {
-//   alert('Sorry, but No');
-// }
-// // console.log(Sorry, but No);
-// console.log('question1 =' + qThree);
+let qThree = prompt('Was I born in Peru').toLowerCase();
+if (qThree === 'yes' || qThree === 'y') {
+  alert('NOO');
+  // console.log('Correct');
+} else if (qThree === 'no' || qThree === 'n') {
+  alert('Good job');
+  score++;
+}
+// console.log(Sorry, but No);
+console.log('question1 =' + qThree);
 
-// let qFour = prompt('Did I ever play soccer').toLowerCase();
-// if (qFour === 'yes' || qFour === 'y') {
-//   alert('Correct');
-//   // console.log('Correct');
-// } else if (qFour === 'no' || qFour === 'n') {
-//   alert('Sorry, but No');
-// }
-// // console.log(Sorry, but No);
-// console.log('question1 =' + qFour);
+let qFour = prompt('Did I ever play soccer').toLowerCase();
+if (qFour === 'yes' || qFour === 'y') {
+  alert('Correct');
+  score++;
+  // console.log('Correct');
+} else if (qFour === 'no' || qFour === 'n') {
+  alert('Sorry, but No');
+}
+// console.log(Sorry, but No);
+console.log('question1 =' + qFour);
 
-// let qFive = prompt('Am I trying to stay in shape?').toLowerCase();
-// if (qFive === 'yes' || qFive === 'y') {
-//   alert('Correct');
-//   // console.log('Correct');
-// } else if (qFive === 'no' || qFive === 'n') {
-//   alert('Sorry, but No');
-// }
-// // console.log(Sorry, but No);
-// console.log('question1 =' + qFive);
+let qFive = prompt('Am I trying to stay in shape?').toLowerCase();
+if (qFive === 'yes' || qFive === 'y') {
+  alert('Correct');
+  score++;
+  // console.log('Correct');
+} else if (qFive === 'no' || qFive === 'n') {
+  alert('Sorry, but No');
+}
+// console.log(Sorry, but No);
+console.log('question1 =' + qFive);
 
 let myAge = '29';
 
@@ -64,7 +70,9 @@ for (let i = 0; i < 4; i++) {
 
   if (qSix === myAge) {
     alert('You`ve played this game before. Good Job I`m 29');
+    score++;
     break;
+
   }
   else if (qSix > myAge) {
     alert('I`m not that old lol');
@@ -76,16 +84,19 @@ for (let i = 0; i < 4; i++) {
     alert(`My age is ${myAge}`);
   }
 
+  let carArray = ['vw', 'ferrari', 'mersedes', 'bmw', 'audi', 'pagani'];
 
+  for (let i = 0; i < 6; i++) {
+    let qSeven = prompt('I got the last one for you. Name Car brand I might like').toLowerCase();
 
-
-
-
-
-
-
-
-
-// alert(`Thanks ${userName} this survey was extremley fun`);
-
+    if (carArray.indexOf(qSeven) !== -1) {
+      alert('You are right');
+      score++;
+      break;
+    }
+    else {
+      alert('Unfortunetly noooo');
+    }
+  }
 }
+alert(`Thanks ${userName} this survey was extremley fun you got ${score} correct answers`);
